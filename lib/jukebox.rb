09e19d0a68b4b966a86aa2bcd9ee4b9489777d8e
github username <github email address>
2019-03-10 00:32:52 -0700

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,52 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
 
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a name or number:"
+  selection = gets.chomp
+  if selection.to_i > 0 && selection.to_i <= songs.length
+    puts "Playing #{songs[selection.to_i - 1]}"
+  elsif songs.include?(selection)
+    puts "Playing #{selection}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    user_input = gets.chomp
+    case user_input
+      when "help"
+        help
+      when "list"
+        list(songs)
+      when "play"
+        play(songs)
+      when "exit"
+        exit_jukebox
+        break
+      else
+        puts "Invalid input, please try again"
+    end
+  end
+end
